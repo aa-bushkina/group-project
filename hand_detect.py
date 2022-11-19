a97ee251fb29e201882010b1ffcbcb3446276090
Alena Bushkina <aa-bushkina@mail.ru>
2022-11-19 19:49:17 +0300

    add starting capturing video from webcam with cv2

--- hand_detect.py
+++ hand_detect.py
@@ -1,6 +1,10 @@
+import cv2
 import mediapipe as mp
 
 
 def run_until_hand_detected():
     mpHands = mp.solutions.hands
-    hands = mpHands.Hands(min_detection_confidence=0.75, min_tracking_confidence=0.75, max_num_hands=2)
+    hands = mpHands.Hands(static_image_mode=False, model_complexity=1, min_detection_confidence=0.75,
+                          min_tracking_confidence=0.75, max_num_hands=2)
+
+    cap = cv2.VideoCapture(0)
