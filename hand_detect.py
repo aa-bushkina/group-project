8fd6b103805c4c87ac008d71df9f4d2ddf1fc3c4
Alena Bushkina <aa-bushkina@mail.ru>
2022-11-19 20:00:54 +0300

    add taking video

--- hand_detect.py
+++ hand_detect.py
@@ -8,3 +8,7 @@ def run_until_hand_detected():
                           min_tracking_confidence=0.75, max_num_hands=2)
 
     cap = cv2.VideoCapture(0)
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
