d73568d85d892ade3e4e95d2eff265ce691d1d8f
Alena Bushkina <aa-bushkina@mail.ru>
2022-11-19 20:12:37 +0300

    make function multithreading

--- hand_detect.py
+++ hand_detect.py
@@ -1,13 +1,18 @@
+import threading
+
 import cv2
 import mediapipe as mp
 
 
-def run_until_hand_detected():
+def run_until_hand_detected(func):
     mpHands = mp.solutions.hands
     hands = mpHands.Hands(static_image_mode=False, model_complexity=1, min_detection_confidence=0.75,
                           min_tracking_confidence=0.75, max_num_hands=2)
 
     cap = cv2.VideoCapture(0)
+
+    print(f"Запустили функцию, которая выключается по обнаружению руки")
+    threading.Thread(target=func, daemon=True).start()
 
     while True:
         success, img = cap.read()
