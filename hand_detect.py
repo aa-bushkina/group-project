91bed5e466374efba2af8c514720377de5fc6404
Alena Bushkina <aa-bushkina@mail.ru>
2022-11-19 20:10:30 +0300

    replace video taking to loop

--- hand_detect.py
+++ hand_detect.py
@@ -6,15 +6,15 @@ def run_until_hand_detected():
     mpHands = mp.solutions.hands
     hands = mpHands.Hands(static_image_mode=False, model_complexity=1, min_detection_confidence=0.75,
                           min_tracking_confidence=0.75, max_num_hands=2)
+    while True:
+        cap = cv2.VideoCapture(0)
+        success, img = cap.read()
+        img = cv2.flip(img, 1)
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(imgRGB)
 
-    cap = cv2.VideoCapture(0)
-    success, img = cap.read()
-    img = cv2.flip(img, 1)
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
-
-    if results.multi_hand_landmarks:
-        #make some signal
-        break
-    cv2.imshow('Image', img)
-    cv2.waitKey(1)
+        if results.multi_hand_landmarks:
+            #make some signal
+            break
+        cv2.imshow('Image', img)
+        cv2.waitKey(1)
