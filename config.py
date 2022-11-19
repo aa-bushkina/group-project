ffc02f8400998a72bcba035d092289fcdc29228c
Ilya <svaikinilya@yandex.ru>
2022-11-19 20:36:47 +0300

    Add set to check number of ports

--- config.py
+++ config.py
@@ -8,3 +8,7 @@ ioPorts = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 29, 3
 groundPorts = [6, 9, 14, 20, 25, 30, 34, 39]
 uselessPorts = [1, 2, 4, 17, 27, 28]
 inUsePorts = []
+
+allPorts = set(ioPorts)
+allPorts.update(groundPorts)
+allPorts.update(uselessPorts)
