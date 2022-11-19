76f83ad916b5d38399ceaa2b19f1d6f8fb645859
stolbovik <stolbov_svyatoslav_02@mail.ru>
2022-11-19 20:36:03 +0300

    add debug function for project

--- utils.py
+++ utils.py
@@ -17,3 +17,37 @@ def outForDebug(boolean):
         print(1, end=' ')
     else:
         print(0, end=' ')
+
+def debugShow(debugPorts):
+    if len(debugPorts) != 12:
+        print('Чето тут не так переделывай')
+        exit(4)
+    for i in range(0, 7):
+        for j in range(0, 7):
+            if i == 0 and j == 3:
+                outForDebug(debugPorts[0].isLightOn())
+            elif i == 1 and j == 4:
+                outForDebug(debugPorts[1].isLightOn())
+            elif i == 2 and j == 5:
+                outForDebug(debugPorts[2].isLightOn())
+            elif i == 3 and j == 6:
+                outForDebug(debugPorts[3].isLightOn())
+            elif i == 4 and j == 5:
+                outForDebug(debugPorts[4].isLightOn())
+            elif i == 5 and j == 4:
+                outForDebug(debugPorts[5].isLightOn())
+            elif i == 6 and j == 3:
+                outForDebug(debugPorts[6].isLightOn())
+            elif i == 5 and j == 2:
+                outForDebug(debugPorts[7].isLightOn())
+            elif i == 4 and j == 1:
+                outForDebug(debugPorts[8].isLightOn())
+            elif i == 3 and j == 0:
+                outForDebug(debugPorts[9].isLightOn())
+            elif i == 2 and j == 1:
+                outForDebug(debugPorts[10].isLightOn())
+            elif i == 1 and j == 2:
+                outForDebug(debugPorts[11].isLightOn())
+            else:
+                print(' ', end=' ')
+        print()
