bebad3a5866e7f5d87b88e3add49780c1cf61dae
Ilya <svaikinilya@yandex.ru>
2022-11-21 01:32:09 +0300

    add IoPort

---
+++ Components/ioPort.py
@@ -0,0 +1,41 @@
+from config import ioPorts, inUsePorts
+
+
+class IoPort(object):
+    def __init__(self, numPort):
+        if (numPort not in ioPorts) or (numPort in inUsePorts):
+            print("err")
+            exit(2)
+        self.__ioPort = numPort
+        self.__voltage = 0
+        inUsePorts.append(numPort)
+        self.__lock = threading.Lock()
+
+    def get(self):
+        if not (self.__ioPort in inUsePorts):
+            print("err")
+            exit(2)
+        return self.__ioPort
+
+    def lightOn(self):
+        self.__lock.acquire()
+        try:
+            if self.__voltage == 1:
+                print('Два раза зажгли одно и то же!!!\n')
+            self.__voltage = 1
+            # IO.output(self.__ioPort, self.__voltage)
+        finally:
+            self.__lock.release()
+
+    def lightOff(self):
+        self.__lock.acquire()
+        try:
+            if self.__voltage == 0:
+                print('Два раза выключили одно и то же!!!\n')
+            self.__voltage = 0
+        # IO.output(self.__ioPort, 0)
+        finally:
+            self.__lock.release()
+
+    def isLightOn(self):
+        return self.__voltage == 1
