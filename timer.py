937a0fb7b3edd20718b316f2fb23cb6037013702
Alena Bushkina <aa-bushkina@mail.ru>
2022-11-22 00:12:02 +0300

    add comments

--- timer.py
+++ timer.py
@@ -20,35 +20,36 @@ def timer(ports, start_minutes, start_seconds):
     is_equals = False
     minutes_port = start_minutes
     sec_port = start_seconds // 5
-    while minutes_port >= 0:
+    while minutes_port >= 0:  # Цикл по всему времени
         if minutes_port != 0:
             ports[minutes_port].lightOn()
-        while sec_port >= 0:
+        while sec_port >= 0:  # Цикл по секундам в рамках одной минуты
             sec = 0
             sec_delta = 5
             if is_first_iteration:
                 sec_delta = start_seconds - (start_seconds // 5) * 5
                 is_first_iteration = False
+
             if sec_port == minutes_port and minutes_port != 0:
                 num_iter = 0
-                while sec < sec_delta:
+                while sec < sec_delta:  # Цикл по 5 секундам (мигание одной лампочки), когда минуты совпали с секундами
                     if num_iter != 0:
                         ports[sec_port].lightOn()
                     num_iter = num_iter + 1
                     is_equals = True
-                    # debugShow(ports)
+                    #debugShow(ports)
                     time.sleep(0.3)
                     ports[sec_port].lightOff()
                     time.sleep(0.7)
                     sec = sec + 1
             else:
-                while sec < sec_delta:
+                while sec < sec_delta:  # Цикл по 5 секундам (мигание одной лампочки)
                     ports[sec_port].lightOn()
-                # debugShow(ports)
-                time.sleep(0.3)
-                ports[sec_port].lightOff()
-                time.sleep(0.7)
-                sec = sec + 1
+                    #debugShow(ports)
+                    time.sleep(0.3)
+                    ports[sec_port].lightOff()
+                    time.sleep(0.7)
+                    sec = sec + 1
             sec_port = sec_port - 1
             if is_equals:
                 ports[minutes_port].lightOn()
@@ -58,7 +59,7 @@ def timer(ports, start_minutes, start_seconds):
             ports[minutes_port].lightOff()
         minutes_port = minutes_port - 1
 
-    print("помашите рукой")
+    print("Помашите рукой")
 
     threading.Timer(interval=1, function=run_until_hand_detected, args=[lambda: endOfTimer(ports)]).start()
 
@@ -73,4 +74,4 @@ def endOfTimer(ports):
     event.clear()
     eventForClock.clear()
     clearPorts(ports)
-    print("конец таймера")
+    print("Конец таймера")
