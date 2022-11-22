a79edafab1d2299d339c12972469eb0262c0da6c
stolbovik <stolbov_svyatoslav_02@mail.ru>
2022-11-22 22:03:15 +0300

    add function for do clock

--- Components/clock.py
+++ Components/clock.py
@@ -13,16 +13,26 @@ def startClock(io_ports_for_clock):
     minuteNow = datetime.datetime.now().time().minute
     ioPorts = io_ports_for_clock
     while not eventForClock.isSet():
+        temp_hours = ioPorts[hourNow % 12]
         if not ioPorts[hourNow % 12].isLightOn():
             ioPorts[hourNow % 12].lightOn()
         if hourNow % 12 != math.floor(minuteNow / 5):
             ioPorts[math.floor(minuteNow / 5)].lightOn()
+        # debugShow(__IoPorts)
         time.sleep(0.5)
         if hourNow % 12 != math.floor(minuteNow / 5):
             ioPorts[math.floor(minuteNow / 5)].lightOff()
+        # debugShow(__IoPorts)
         time.sleep(0.5)
         minuteNow = datetime.datetime.now().time().minute
         hourNow = datetime.datetime.now().time().hour
+        if ioPorts[hourNow % 12].get() != temp_hours.get():
+            temp_hours.lightOff()
     event.clear()
     eventForClock.clear()
     clearPorts(io_ports_for_clock)
+
+
+def doClock(ports):
+    threading.Timer(interval=1, function=run_until_hand_detected, args=[lambda: startClock(ports)]).start()
+    print("Помашите рукой если хотите остановить часы")
