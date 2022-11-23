a435686397e3bdec1eaf9312f862a66e252a970e
stolbovik <stolbov_svyatoslav_02@mail.ru>
2022-11-23 18:46:07 +0300

    add function for secundomer with debug

--- Components/secundomer.py
+++ Components/secundomer.py
@@ -4,3 +4,22 @@ import time
 from config import event, eventForClock
 from hand_detect import run_until_hand_detected
 from utils import debugShow, clearPorts
+
+
+def secundomer(ports, timetostop):
+    sec = 0
+    while True:
+        for i in range(0, 12):
+            for j in range(0, 5):
+                ports[i].lightOn()
+                debugShow(ports)
+                time.sleep(0.5)
+                sec += 1
+                ports[i].lightOff()
+                time.sleep(0.5)
+                debugShow(ports)
+                if sec == timetostop:
+                    event.clear()
+                    eventForClock.clear()
+                    clearPorts(ports)
+                    return
