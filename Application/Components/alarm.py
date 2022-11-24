90dd45d39b1a23811a99a4a0ae628376926c5b9f
Alena Bushkina <aa-bushkina@mail.ru>
2022-11-24 08:49:18 +0300

    add setUpAlarm

--- Application/Components/alarm.py
+++ Application/Components/alarm.py
@@ -6,6 +6,15 @@ from config import event, eventForClock
 from hand_detect import run_until_hand_detected
 from utils import clearPorts
 
+def setUpAlarm(ports, alarm_time_raw):
+    if isinstance(alarm_time_raw, str):
+        alarm_time_parsed = datetime.datetime.strptime(alarm_time_raw, '%d %H:%M')
+        alarm_time = datetime.datetime.now().replace(day=alarm_time_parsed.day, hour=alarm_time_parsed.hour,
+                                                     minute=alarm_time_parsed.minute)
+    else:
+        alarm_time = alarm_time_raw
+    delay = alarm_time - datetime.datetime.now()
+
 def alarm(ports):
     blink_interval = 0.3
     delay_next = 0.1
