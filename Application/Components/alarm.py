8bf6f7c2e226a2f880234e96b5b2bfb5c4da8345
Alena Bushkina <aa-bushkina@mail.ru>
2022-11-24 08:51:05 +0300

    add threading start od alarm

--- Application/Components/alarm.py
+++ Application/Components/alarm.py
@@ -18,6 +18,8 @@ def setUpAlarm(ports, alarm_time_raw):
         print(f"Будильник: неправильно передано время {alarm_time_parsed}")
         return
     print(f"Будильник: успешно установлен на время {alarm_time}. Зазвонит через {delay.total_seconds()} сек")
+    #threading.Thread().start() #FIXME пример запуска вашей функции с выключением по взмаху руки
+    threading.Timer(interval=delay.total_seconds(), function=run_until_hand_detected, args=[lambda: alarm(ports)]).start()
 
 def alarm(ports):
     blink_interval = 0.3
